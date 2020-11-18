6c7e0f2afdd5f0710c19b51b1b0c2c45fb68e124
Caitlin Burke <cmb138@email.latech.edu>
2020-11-17 19:23:39 -0600

    Dialogue commit 2

--- Project/MyGameLibrary/Enemy.cs
+++ Project/MyGameLibrary/Enemy.cs
@@ -1,13 +1,14 @@
 ﻿using System.Drawing;
 
 namespace Fall2020_CSC403_Project.code {
-  /// <summary>
-  /// This is the class for an enemy
-  /// </summary>
-  public class Enemy : BattleCharacter {
     /// <summary>
-    /// THis is the image for an enemy
+    /// This is the class for an enemy
     /// </summary>
+    public class Enemy : BattleCharacter {
+        /// <summary>
+        /// THis is the image for an enemy
+        /// </summary>
+    public static string[] enemyResponses = new string[5] { "Please, have mercy", "No. Stop.", "Ah", "Oh no", "How could you do this?" };
     public Image Img { get; set; }
 
     /// <summary>
